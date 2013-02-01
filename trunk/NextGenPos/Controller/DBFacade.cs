133690e644b958a5ec63f79019f3675d011b4259
moku@eal.dk <moku@eal.dk@a80d47db-3586-c35b-98ca-a94ddfe70628>
2013-02-01 08:28:01 +0000

    A mockup of nextgen

---
+++ trunk/NextGenPos/Controller/DBFacade.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient; // Inkluder for DB adgang
+
+namespace Controller
+{
+    public class DBFacade
+    {
+        string ConnectionString { get; set; }
+        SqlConnection dbconn;
+
+        public DBFacade(string connectionstring)
+        {
+            ConnectionString = connectionstring;
+        }
+
+        public void connectDB() {
+            dbconn = new SqlConnection(ConnectionString);
+            dbconn.Open();
+            Console.WriteLine("Sql server version " + dbconn.ServerVersion);
+        }
+
+        public void closeDB() {
+            dbconn.Close();
+        }
+
+
+    }
+}
---
+++ trunk/NextGenPos/Controller/Facade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Controller
+{
+    public class Facade
+    {
+        DBFacade dbf;
+
+        public Facade() {
+            dbf = new DBFacade("Data Source=10.165.150.520;Initial Catalog=nextgenPOS;Persist Security Info=True;User ID=dm122;Password=dm122");
+            dbf.connectDB();
+            dbf.closeDB();
+
+        }
+    }
+}
