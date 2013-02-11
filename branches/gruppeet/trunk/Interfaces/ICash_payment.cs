a97367f4a4830576c60286de7af7d9166e0c5aa4
moku@eal.dk <moku@eal.dk@a80d47db-3586-c35b-98ca-a94ddfe70628>
2013-02-11 10:34:46 +0000

    Søren har ændret ICash_payment.cs har indsat properties

--- branches/gruppeet/trunk/Interfaces/ICash_payment.cs
+++ branches/gruppeet/trunk/Interfaces/ICash_payment.cs
@@ -8,5 +8,9 @@ namespace Interfaces
     class ICash_payment
     {
 
+        int _cashPaymentID { get; }
+        decimal _amountTendered { get; set; }
+        decimal _cashBack { get; set; }
+      
     }
 }
