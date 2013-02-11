7620ee67e5240e8916fe0ff424d1dfe7988b4ca8
moku <moku@eal.dk>
2013-02-11 10:37:08 +0000

    Laves store, storecol og interface

--- Model/Store.cs
+++ Model/Store.cs
@@ -7,6 +7,20 @@ namespace NextGenPOSModel
 {
     public class Store
     {
-        //test
+        int storeID, product_catalog_ID;
+        string store_name, store_address;
+
+        public Store(int storeid, int productcatID, string storename, string address) 
+        {
+            this.storeID = storeid;
+            this.product_catalog_ID = productcatID;
+            this.store_name = storename;
+            this.store_address = address;
+        }
+
+        public int StoreID { get; set; }
+        public int ProductCatalogID { get; set; }
+        public string Storename { get; set; }
+        public string StoreAddress { get; set; }
     }
 }
