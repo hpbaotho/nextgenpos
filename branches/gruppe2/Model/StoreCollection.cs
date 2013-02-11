c01b9ee6892c20cb58bed85053463f3f112514f6
moku@eal.dk <moku@eal.dk@a80d47db-3586-c35b-98ca-a94ddfe70628>
2013-02-11 10:37:08 +0000

    Laves store, storecol og interface

---
+++ branches/gruppe2/Model/StoreCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NextGenPOSModel
+{
+    public class StoreCollection
+    {
+        List<Store> listeafstores;
+
+        public StoreCollection() 
+        {
+            listeafstores = new List<Store>();
+        }
+
+        public List<Store> ListeafStores { get; set; }
+    }
+}
