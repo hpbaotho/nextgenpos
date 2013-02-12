358a90c2fd1a6ccf2bb9f5ceb5112e6d1c4cb95c
moku@eal.dk <moku@eal.dk@a80d47db-3586-c35b-98ca-a94ddfe70628>
2013-02-12 10:28:54 +0000

    Added interfacefiles

---
+++ trunk/Interfaces/ISalesLineItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interfaces
+{
+    public interface ISalesLineItem
+    {
+        int Id { get; }
+        int Quantity { get; }
+        int Sale_Id { get; }
+        int Item_Id { get; }
+    }
+}
---
+++ trunk/Interfaces/IStore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interfaces
+{
+    public interface IStore
+    {
+        int StoreID { get; set; }
+        int ProductCatalogID { get; set; }
+        string Storename { get; set; }
+        string StoreAddress { get; set; }
+    }
+}
